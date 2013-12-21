5bda50d9b360aaa21d76feea8712c41d725139fe
Trevor Austin <taustin@civisanalytics.com>
2013-12-21 00:19:40 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cakesofwrath::Application.config.secret_key_base = 'ca896e372573045285a5e319b4526b4a2cf696cd6e820fe3353c9d040b0a27dca264295e7f23bf2745209a82b14efc49f4753945263c33921f711712cc48241a'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Cakesofwrath::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  root 'welcome#index'
+
+end
